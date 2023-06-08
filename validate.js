6180665c97bc6d4f831cf64c22a9cb24b0d0b63d
Santiago <sprietogo@unbosque.edu.co>
2023-06-08 12:07:05 -0500

    sdsadfdsaDSadgdddsdsdd

--- validate.js
+++ validate.js
@@ -1,23 +1,19 @@
-    const token = localStorage.getItem("token");
-    const usuario = localStorage.getItem("usuario");
-  
-    if (token && usuario) {
-      // Construir la URL del endpoint con el usuario y el token
-      const url = `https://apex.oracle.com/pls/apex/pangolin/store/token/${usuario}/${token}`;
-  
-      // Realizar la petición GET usando fetch
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          if (data.items && data.items.length > 0) {
-            // El objeto "items" contiene elementos, no hacer nada
-          } else {
-            // El objeto "items" está vacío, redirigir a login.html
-            window.location.href = "login.html";
-          }
-        })
-        .catch(error => {
-          console.error("Error al realizar la petición:", error);
-        });
-    }
+const token = localStorage.getItem("token");
+const usuario = localStorage.getItem("usuario");
+  // Construir la URL del endpoint con el usuario y el token
+  const url = `https://apex.oracle.com/pls/apex/pangolin/store/token/${usuario}/${token}`;
+  // Realizar la petición GET usando fetch
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      if (data.items && data.items.length > 0) {
+        // El objeto "items" contiene elementos, no hacer nada
+      } else {
+        // El objeto "items" está vacío, redirigir a login.html
+        window.location.href = "login.html";
+      }
+    })
+    .catch(error => {
+      console.error("Error al realizar la petición:", error);
+    });
