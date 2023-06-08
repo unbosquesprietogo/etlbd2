9b95bf42942830b6845a23cad40b4c38e9d17354
Santiago <sprietogo@unbosque.edu.co>
2023-06-08 16:09:35 -0500

    producto2

--- producto.js
+++ producto.js
@@ -1,41 +1,44 @@
-$(document).ready(function() {
-      // Obtener los datos mediante una petición AJAX al endpoint
-      $.ajax({
-        url: 'https://apex.oracle.com/pls/apex/pangolin/store/inventory/',
-        type: 'GET',
-        success: function(response) {
-          // Obtener los productos del response JSON
-          var products = response.items;
-
-          // Construir las filas de la tabla con los productos
+  $(document).ready(function() {
+    // Realizar la petición AJAX al endpoint de productos
+    $.ajax({
+      url: 'endpoint_de_productos',
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        if (data && data.length > 0) {
+          // Construir las filas de la tabla con los datos de los productos
           var tableRows = '';
-          for (var i = 0; i < products.length; i++) {
-            var product = products[i];
-            tableRows += '<tr>';
-            tableRows += '<td>' + product.id_producto + '</td>';
-            tableRows += '<td>' + product.codigo_barras + '</td>';
-            tableRows += '<td>' + product.nombre_producto + '</td>';
-            tableRows += '<td>' + product.descripcion + '</td>';
-            tableRows += '<td>' + product.precio_unitario + '</td>';
-            tableRows += '<td>' + product.cantidad_stock + '</td>';
-            tableRows += '</tr>';
+          for (var i = 0; i < data.length; i++) {
+            var product = data[i];
+            var row = '<tr>' +
+              '<td>' + product.id_producto + '</td>' +
+              '<td>' + product.codigo_barras + '</td>' +
+              '<td>' + product.nombre_producto + '</td>' +
+              '<td>' + product.descripcion + '</td>' +
+              '<td>' + product.precio_unitario + '</td>' +
+              '<td>' + product.cantidad_stock + '</td>' +
+              '</tr>';
+            tableRows += row;
           }
-
-          // Insertar las filas en la tabla
           $('#productTableBody').html(tableRows);
-        },
-        error: function(xhr, status, error) {
-          console.log('Error al obtener los productos:', error);
+        } else {
+          console.log('No se encontraron productos');
         }
-      });
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log('Error al obtener los productos:', errorThrown);
+      }
+    });
 
-      // Configurar el evento click del botón de búsqueda
-      $('#searchButton').click(function() {
-        var searchTerm = $('#searchInput').val().toLowerCase();
+    // Configurar el evento click del botón de búsqueda
+    $('#searchButton').click(function() {
+      var searchTerm = $('#searchInput').val().toLowerCase();
+      var searchColumn = $('#searchColumn').val();
 
-        // Filtrar los productos por el término de búsqueda en las columnas
-        $('#productTableBody tr').filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(searchTerm) > -1);
-        });
+      // Filtrar los productos por el término de búsqueda en la columna seleccionada
+      $('#productTableBody tr').filter(function() {
+        var columnText = $(this).find('td:eq(' + searchColumn + ')').text().toLowerCase();
+        $(this).toggle(columnText.indexOf(searchTerm) > -1);
       });
     });
+  });
